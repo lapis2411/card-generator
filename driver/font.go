30ff546ab3b1100b5d9f74ebad6333c1c7ff5dbd
lapis2411 <lapis2411@gmail.com>
2024-01-29 00:52:50 +0900

    [feat] フォント、文字幅を考慮して自動で折り返しできるように

---
+++ driver/font.go
@@ -0,0 +1,41 @@
+package driver
+
+import (
+	"fmt"
+	"golang.org/x/image/draw"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+	"image"
+)
+
+func DrawStringWithWrap(d *font.Drawer, s string, w int) {
+	originX := d.Dot.X
+	progress := fixed.Int26_6(0)
+	width := fixed.I(w)
+
+	prevC := rune(-1)
+	met := d.Face.Metrics()
+	for _, c := range s {
+		if prevC >= 0 {
+			d.Dot.X += d.Face.Kern(prevC, c)
+		}
+		advance, ok := d.Face.GlyphAdvance(c)
+		if !ok {
+			c = '?'
+			advance, _ = d.Face.GlyphAdvance(c)
+		}
+		fmt.Printf("%v\n", advance.Round())
+		progress += advance
+		if progress > width {
+			d.Dot.X = originX
+			d.Dot.Y += met.Height
+			progress = 0
+		}
+		dr, mask, maskp, advance, _ := d.Face.Glyph(d.Dot, c)
+		if !dr.Empty() {
+			draw.DrawMask(d.Dst, dr, d.Src, image.Point{}, mask, maskp, draw.Over)
+		}
+		d.Dot.X += advance
+		prevC = c
+	}
+}
